136ac2c3ff312a532f0a0708c078aa2dda7c0c78
Kaustav <kaustav86x@gmail.com>
2023-03-25 16:38:13 +0530

    C#-Basics
    Arrays, loops, switch-case, regex, datetime and many more basic concepts of C# !

---
+++ C-sharp-Basics/C-sharp-Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string content = System.IO.File.ReadAllText(@"C:\Users\HP\Downloads\input.txt");
+            Console.WriteLine(content);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstCS/FirstCS/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCS
+{
+    class Class1
+    {
+        // using the concepts of reading file 
+        public static void main(string[] args)
+        {
+            string filecontent = System.IO.File.ReadAllText("@C:/Users/HPDownloads/input.txt");
+            Console.WriteLine(filecontent);
+
+            Console.ReadLine();
+        }
+    }
+}
