f7c795df8dec46358b113293f484813cc6be73a0
Kaustav <kaustav86x@gmail.com>
2023-04-02 00:00:31 +0530

    Inheritance and Polymorphism Basics

---
+++ C-sharp-Basics/OOPS_5/Insurance.cs
@@ -0,0 +1,113 @@
+﻿/*using System;*/
+namespace InsuranceDetails
+{
+	public class Insurance
+	{
+		private string insuraneNo;
+		private string insuranceName;
+		private double amountCovered;
+
+		public string InsuranceNO
+		{
+			get { return this.insuranceNo; }
+			set { this.insuraneNo = value; }
+		}
+		public string InsuranceName
+		{
+			get { return this.insuranceName; }
+			set { this.insuraneName = value; }
+		}
+
+		public double AmountCovered
+		{
+			get { return this.amountCovered; }
+			set { this.amountCovered = value; }
+		}
+		// default constructor
+		public Insurance()
+		{
+
+		}
+		// parameterized Constructor
+		public Insurance(string insuranceNo, string insuranceName, double amountCovered)
+		{
+			InsuranceNo = insuranceNo;
+			InsuranceName = insuranceName;
+			AmountCovered = amountCovered;
+		}
+
+		public virtual double calculatePremium()
+		{
+			return 0;
+		}
+	}
+
+	class MotorInsurance : Insurance
+	{
+		private double idv;
+		private float depPercent;
+
+		public double Idv
+		{
+			get { return this.idv; }
+			set { this.idv = value; }
+		}
+		public float DepPercent
+		{
+			get { return this.depPercent; }
+			set { this.depPercent = value; }
+		}
+
+		public MotorInsurance()
+		{
+
+		}
+		public MotorInsurance(string insuranceNo, string insuranceName, double amountCovered, float depPercent) : base(depPercent)
+		{
+			Idv = idv;
+			InsuranceNO = insuranceNo;
+			InsuranceName = insuranceName;
+			AmountCovered = amountCovered;
+			DepPercent = depPercent;
+		}
+
+		public override double calculatePremium()
+		{
+			double Mpremimum;
+			Idv = (AmountCovered - (AmountCovered * DepPercent) / 100);
+			Mpremium = (Idv * 3) / 100;
+			return Mpremimum;
+		}
+	}
+
+	class LifeInsurance : Insurance
+	{
+		private int policyTerms;
+		private float benefitPercentage;
+
+		public int PolicyTerms
+		{
+			get { return this.policyTerms; }
+			set { this.policyTerms = value; }
+		}
+
+		public float BenefitPercentage
+		{
+			get { return this.benefitPercentage; }
+			set { this.benefitPercentage = value; }
+		}
+
+		public LifeInsurance(string insuranceNo, string insuranceName, double amountCovered, int policyTerms, float benefitPrecentage) : base(policyTerms, benefitPrecentage)
+		{
+			PolicyTerms = policyTerms;
+			BenefitPercentage = benefitPrecentage;
+		}
+
+		public override double calculatePremium()
+		{
+			double Lpremimum;
+			Lpremimum = (AmountCovered - ((AmountCovered * BenefitPercentage) / 100)) / policyTerms;
+			return Lpremimum;
+		}
+	}
+}
--- C-sharp-Basics/OOPS_5/Program.cs
+++ C-sharp-Basics/OOPS_5/Program.cs
@@ -1,7 +1,76 @@
-﻿public class Program
+﻿namespace InsuranceDetails
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        /*Console.WriteLine("Hello, World!");*/
+        public double addPolicy(Insurance ins, int opt)
+        {
+            if (opt == 1)
+            {
+                return ins.calculatePremium();
+            }
+            else if (opt == 2)
+            {
+                return ins.calculatePremium();
+            }
+            return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            int choice;
+            int polterms;
+            int benprecent;
+            int depercent;
+            double amnt;
+            double result;
+            string iname;
+            string inum;
+
+            /*Insurance I = new Insurance();*/
+            Program p = new Program();
+            Console.WriteLine("Insurance Number");
+            inum = Console.ReadLine();
+
+            Console.WriteLine("Insurance Name");
+            iname = Console.ReadLine();
+
+            Console.WriteLine("Amount Covered");
+            amnt = Convert.ToDouble(Console.ReadLine());
+
+            Insurance I = new Insurance(inum, iname, amnt);
+
+            Console.WriteLine("Select\n\n1.Life Insurance\n\n2.Motor Insurance\n");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Policy Terms : ");
+                    polterms = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Benefit Percent : ");
+                    benprecent = Convert.ToInt32(Console.ReadLine());
+
+                    Insurance li = new LifeInsurance(inum, iname, amnt, polterms, benprecent);
+
+                    result = p.addPolicy(li, 1);
+                    Console.WriteLine("Calculated Premium : {0}", result);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Depreciation Percent : ");
+                    depercent = Convert.ToInt32(Console.ReadLine());
+
+                    Insurance mi = new MotorInsureance(inum, iname, amnt, depercent);
+
+                    result = p.addPolicy(mi, 2);
+
+                    Console.WriteLine("Calculated Premium : {0}", result);
+                    break;
+
+                case 3: Console.WriteLine("Invalid Option !!");
+            }
+
+        }
     }
 }
