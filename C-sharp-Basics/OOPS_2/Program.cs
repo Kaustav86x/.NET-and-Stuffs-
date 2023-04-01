6f0d3c7f0ed395a43932a70d7f763fb70f09b476
Kaustav <kaustav86x@gmail.com>
2023-04-01 17:27:37 +0530

    OOPS basics pt-2

---
+++ C-sharp-Basics/OOPS_2/Program.cs
@@ -0,0 +1,63 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // declaration and initialised to space
+        string name = "";
+        DateTime dt;
+        string jd = "";
+        Console.WriteLine("Enter the name");
+        name = Console.ReadLine();
+        Console.WriteLine("Enter the datetime");
+        dt = Convert.ToDateTime(Console.ReadLine());
+        Console.WriteLine("Enter the job description");
+        jd = Console.ReadLine();
+
+        // parameterized constructor
+        Domain dm = new Domain(name, dt, jd);
+        {
+            this.name = name;
+            this.dt = new DateTime();
+        }
+    }
+}
+
+public class Domain
+{
+    private string name;
+    private DateTime date;
+    private string jobdc;
+
+    // string daat = date.ToString();
+
+    // properties 
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public DateTime Date
+    {
+        get { return date; }
+        set { date = value; }
+    }
+
+    public string Jobdc
+    {
+        get { return jobdc; }
+        set { jobdc = value; }
+    }
+
+    public Domain()
+    {
+    }
+
+    public Domain(string name, DateTime date, string jobdc)
+    {
+        this.name = name;
+        this.date = date;
+        this.jobdc = jobdc;
+    }
+}
+
