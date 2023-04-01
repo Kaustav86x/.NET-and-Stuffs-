10f04733df9ad610a8a431f9ca6bd556b7f86fbb
Kaustav <kaustav86x@gmail.com>
2023-04-01 17:40:26 +0530

    OOPS basics pt-5

---
+++ C-sharp-Basics/DateTime_1/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        DateTime d1 = DateTime.Now; // current date
+        DateTime d2 = new DateTime(2000, 11, 22); // my dob :)
+        TimeSpan span = d1 - d2; // subtraction of two dates resulting in a timespan type of variable 
+        Console.WriteLine(span.ToString()); // gives a sting of days, hour, minutes etc :)
+        Console.WriteLine(span.Days); // 8165 days 
+        Console.WriteLine("My age is {0} years !", span.Days/365);
+    }
+}
