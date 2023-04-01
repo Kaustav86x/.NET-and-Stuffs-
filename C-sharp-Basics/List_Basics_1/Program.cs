d4b8e934f93eaf416c3a987c73e64105b682ea4a
Kaustav <kaustav86x@gmail.com>
2023-04-02 00:59:23 +0530

    Datastructure (List Basics pt-1)

---
+++ C-sharp-Basics/List_Basics_1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace List_Basics_1 
+{
+    public class Program
+    {
+        public static List<Student> StudentList { get; set; } = new List<Student>(); // most important line
+        public static void Main(string[] args)
+        {
+            while (true) 
+            {
+                Console.WriteLine("Choose the option");
+
+                Console.WriteLine("1.Add the student details");
+                Console.WriteLine("2.Get student name by phone");
+                Console.WriteLine("3.Remove student details");
+                Console.WriteLine("4.Exit");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch(option)
+                {
+                    case 1: Console.WriteLine("Enter stduent name");
+                            string name = Console.ReadLine();
+
+                        Console.WriteLine("Enter date of birth");
+                        string dob = Console.ReadLine();
+
+                        Console.WriteLine("Enter phone number");
+                        long phone = Convert.ToInt64(Console.ReadLine());
+
+                        Console.WriteLine("Enter the city");
+                        string city = Console.ReadLine();
+
+                        Student student = new Student(name, dob, phone, city);
+
+                        StudentList.Add(student);
+
+                        Console.WriteLine("{0} {1} {2} {3}",name,dob,phone,city);
+                        break;
+
+                    case 2: Console.WriteLine("Enter the phone number");
+                            long pnum = Convert.ToInt64(Console.ReadLine());
+                            
+                }
+            }
+            
+        }
+    }
+}
---
+++ C-sharp-Basics/List_Basics_1/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Basics_1
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public string DOB { get; set; }
+        public long PhoneNo { get; set; }
+        public string City { get; set; }
+
+        public Student() { }
+
+        public Student(string sname, string dob, long phone, string city) 
+        { 
+            StudentName = sname;
+            DOB = dob;
+            PhoneNo = phone;
+            City = city;
+        }
+
+        public List<Student> AddStudentDetails(string name, string dob, long phone, string city)
+        {
+
+        }
+
+        public string GetStudentName(long phone)
+        {
+
+        }
+
+        public List<Student> RemoveStudentDetails(long phone)
+        {
+
+        }
+    }
+}
