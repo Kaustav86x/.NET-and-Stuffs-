85bb7ddeea6cae971f9297137f9b9946d6984187
Kaustav <kaustav86x@gmail.com>
2023-04-02 23:35:48 +0530

    Application of Inheritance

---
+++ C-sharp-Basics/OOPS_6/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_6
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public long SSN { get; set; } 
+        public string City { get; set; }
+        public double LoanAmount { get; set; }
+        public int NoOfYears { get; set; }
+    }
+}
