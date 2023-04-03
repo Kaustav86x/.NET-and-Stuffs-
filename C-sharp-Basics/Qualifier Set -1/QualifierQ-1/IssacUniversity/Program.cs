baf7bbfa31b22c2c88f8281f46c052a51e586ea7
Kaustav <kaustav86x@gmail.com>
2023-04-03 20:43:15 +0530

    Qualifier Set -1 C# Codes

---
+++ C-sharp-Basics/Qualifier Set -1/QualifierQ-1/IssacUniversity/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace IssacUniversity
+{
+    public class Program
+    {
+        public static List<Student> StudentList { get; set; } = new List<Student>(); // most important line
+        public static void Main(string[] args)
+        {
+            Program prog = new Program();
+            Student st = new Student();
+            while (true)
+            {
+                Console.WriteLine("Choose the option");
+
+                Console.WriteLine("1.Add the student details");
+                Console.WriteLine("2.Get student name by phone");
+                Console.WriteLine("3.Remove student details");
+                Console.WriteLine("4.Exit");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter stduent name");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Enter date of birth");
+                        string dob = Console.ReadLine();
+
+                        Console.WriteLine("Enter phone number");
+                        long phone = Convert.ToInt64(Console.ReadLine());
+
+                        Console.WriteLine("Enter the city");
+                        string city = Console.ReadLine();
+
+                        st.AddStudentDetails(name, dob, phone, city);
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter the phone number");
+                        long pnum = Convert.ToInt64(Console.ReadLine());
+
+                        string sname = st.GetStudentName(pnum);
+                        Console.WriteLine("{0}", sname);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the phone number");
+                        pnum = Convert.ToInt64(Console.ReadLine());
+
+                        List<Student> list = new List<Student>();
+                        list = st.RemoveStudentDetails(pnum);
+                        foreach (Student student in list)
+                        {
+                            Console.WriteLine("{0} {1} {2} {3}", student.StudentName, student.DOB, student.PhoneNo, student.City);
+                        }
+                        break;
+                    case 4: return;
+                }
+            }
+
+        }
+    }
+}
---
+++ C-sharp-Basics/Qualifier Set -1/QualifierQ-1/IssacUniversity/Student.cs
@@ -0,0 +1,64 @@
+﻿using IssacUniversity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssacUniversity
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public string DOB { get; set; }
+        public long PhoneNo { get; set; }
+        public string City { get; set; }
+
+        public Student() { }
+
+        public Student(string sname, string dob, long phone, string city)
+        {
+            StudentName = sname;
+            DOB = dob;
+            PhoneNo = phone;
+            City = city;
+        }
+
+        /*Program prog = new Program();*/
+
+        public List<Student> AddStudentDetails(string name, string dob, long phone, string city)
+        {
+            Student student = new Student(name, dob, phone, city);
+            Program.StudentList.Add(student);
+            return Program.StudentList;
+        }
+
+        public string GetStudentName(long phone)
+        {
+            string result = "";
+            foreach (var getname in Program.StudentList)
+            {
+                if (phone == getname.PhoneNo)
+                {
+                    result = getname.StudentName;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public List<Student> RemoveStudentDetails(long phone)
+        {
+            for (int i = 0; i < Program.StudentList.Count; i++)
+            {
+                if (Program.StudentList[i].PhoneNo == phone)
+                {
+                    Program.StudentList.RemoveAt(i);
+                }
+            }
+            return Program.StudentList;
+        }
+
+
+    }
+}
