25532b9e1c67f06003f82b21ebfc885dbadee68c
Kaustav <kaustav86x@gmail.com>
2023-04-05 02:57:09 +0530

    Application of Regex in place of for loop

--- C-sharp-Basics/Qualifier Set - 2/Question-3/Vegetable_Cost/Program.cs
+++ C-sharp-Basics/Qualifier Set - 2/Question-3/Vegetable_Cost/Program.cs
@@ -1,8 +1,29 @@
-﻿public class Program
+﻿using Vegetable_Cost;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        /*Console.WriteLine("Hello, World!");*/
+        double result = 0.0;
+        Service srvc = new Service();
 
+        /*Console.WriteLine("Hello, World!");*/
+        Console.WriteLine("Enter bill id");
+        srvc.BillId = Console.ReadLine();
+        if(srvc.ValidateBillId())
+        {
+            Console.WriteLine("Enter Vegetable name");
+            srvc.Name = Console.ReadLine();
+            Console.WriteLine("Enter pack capacity in grams");
+            srvc.GramsInPack = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter cost per pack");
+            srvc.CostPerPack = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter quantity to purchase in kgs");
+            float q = float.Parse(Console.ReadLine());
+            result = srvc.CalculateTotalCost(q);
+            Console.WriteLine("Vegetable cost Rs.{0}", result);
+        }
+        else
+            Console.WriteLine("Invalid bill id");
     }
 }
--- C-sharp-Basics/Qualifier Set - 2/Question-3/Vegetable_Cost/Service.cs
+++ C-sharp-Basics/Qualifier Set - 2/Question-3/Vegetable_Cost/Service.cs
@@ -2,18 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Vegetable_Cost
 {
     public class Service : Vegetable
     {
-        public Service(string billId, string name, int gramsInPack, double costPerPack)
+        public Service() { }
+        /*public Service(string billId, string name, int gramsInPack, double costPerPack)
         {
             BillId = billId;
             Name = name;
             GramsInPack = gramsInPack;
             CostPerPack = costPerPack;
+        }*/
+
+        public bool ValidateBillId()
+        {
+            int vBil = 0;
+            bool digi_check = false;
+            if (BillId.Length == 7)
+            {
+                for(int i = 0; i < BillId.Substring(0, 3).Length; i++)
+                {
+                    if (char.IsNumber(BillId.Substring(0, 3).ElementAt(i)))
+                        digi_check = true;
+                    else
+                        digi_check = false;
+                }
+                bool char_check = false;
+                if (Regex.IsMatch(BillId.Substring(4, 3), "^[A-Z]*$"))
+                    char_check = true;
+                else
+                    char_check = false;
+                if (digi_check && char_check && BillId.ElementAt(3) == '-')
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        public double CalculateTotalCost(float quantity)
+        {
+            double Total_cost = (CostPerPack * (quantity * 1000) / GramsInPack);
+            return Total_cost;
         }
 
     }
