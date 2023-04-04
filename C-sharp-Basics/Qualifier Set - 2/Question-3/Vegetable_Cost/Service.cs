8209c17ff3ba456b482e5f4aa7f3933175c0f114
Kaustav <kaustav86x@gmail.com>
2023-04-05 01:35:42 +0530

    Qualifier Set - 2 Q - 3

---
+++ C-sharp-Basics/Qualifier Set - 2/Question-3/Vegetable_Cost/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vegetable_Cost
+{
+    public class Service : Vegetable
+    {
+        public Service(string billId, string name, int gramsInPack, double costPerPack)
+        {
+            BillId = billId;
+            Name = name;
+            GramsInPack = gramsInPack;
+            CostPerPack = costPerPack;
+        }
+
+    }
+}
---
+++ C-sharp-Basics/Qualifier Set - 2/Question-3/Vegetable_Cost/Vegetable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vegetable_Cost
+{
+    public class Vegetable
+    {
+        public string BillId { get; set; }
+        public string Name { get; set; }
+        public int GramsInPack { get; set; }
+        public double CostPerPack { get; set; }
+
+        public Vegetable() { }
+
+        /*public Vegetable() { }*/
+    }
+    
+
+}
