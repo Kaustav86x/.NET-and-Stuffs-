aad7d88caa78d3bc4f112c3f54d7bf0d2446a19b
Kaustav <kaustav86x@gmail.com>
2023-04-08 03:02:07 +0530

    Application of Queue

---
+++ C-sharp-Basics/Qualifier Set - 3/Question-2/Mystic_Beauty_Parlour/Customer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystic_Beauty_Parlour
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public long MobileNumber { get; set; }
+        public string City { get; set; }
+        public string Service { get; set; }
+
+
+        public Customer() { }   
+        public Customer(string customerName, long mobileNumber, string city, string service)
+        {
+            CustomerName = customerName;
+            MobileNumber = mobileNumber;
+            City = city;
+            Service = service;
+        }
+
+        public bool AddCustomer(string name, long mobileNo, string city, string service)
+        {
+            /*Program pr = new Program();*/
+            // object being created
+            Customer cust = new Customer(name, mobileNo, city, service);
+            Program.CustomerQueue.Enqueue(cust);
+            if(Program.CustomerQueue.Count > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public string GetCustomerNameWithService()
+        {
+            Customer custname = Program.CustomerQueue.Peek();
+            string cname = custname.CustomerName.ToUpper();
+            string ser = custname.Service.ToLower();
+
+            return (cname.Trim()+ser.Trim());
+        }
+
+        public Queue<Customer> RemoveCustomerDetails(long mobileNo)
+        {
+            /*Queue<Customer> cstdetails = new Queue<Customer>();*/
+            /*Customer cst = new Customer();*/
+            foreach(Customer c in Program.CustomerQueue)
+            {
+                if (c.MobileNumber == mobileNo)
+                    /*cst = Program.CustomerQueue.Dequeue();*/
+                    /*cstdetails.Enqueue(c);*/
+                    /*cstdetails.Enqueue(Program.CustomerQueue.Dequeue());*/
+                    Program.CustomerQueue.Dequeue();
+            }
+            /*cstdetails.Enqueue(cst);*/
+            return Program.CustomerQueue;
+        }
+    }
+}
---
+++ C-sharp-Basics/Qualifier Set - 3/Question-2/Mystic_Beauty_Parlour/Program.cs
@@ -0,0 +1,71 @@
+﻿using Mystic_Beauty_Parlour;
+
+public class Program
+{
+    public static Queue<Customer> CustomerQueue { get; set; } = new Queue<Customer>();
+    public static void Main(string[] args)
+    {
+        Customer cust = new Customer();
+        int choice;
+        /*Console.WriteLine("Hello, World!");*/
+        while (true) 
+        {
+            Console.WriteLine("Choose the option");
+            Console.WriteLine("1.Add the customer details");
+            Console.WriteLine("2.Get customer name with service");
+            Console.WriteLine("3.Remove customer details");
+            Console.WriteLine("4.Exit");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the customer name");
+                    string cname = Console.ReadLine();
+                    Console.WriteLine("Enter the mobile number");
+                    long mno = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter the city");
+                    string ct = Console.ReadLine();
+                    Console.WriteLine("Enter the service");
+                    string srvc = Console.ReadLine();
+                    bool conf = cust.AddCustomer(cname, mno, ct, srvc);
+                    if(conf)
+                        Console.WriteLine("Customer details added successfully");
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the customer name with service");
+                    string srv = Console.ReadLine();
+                    string result = cust.GetCustomerNameWithService();
+                    Console.WriteLine(result);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter the mobile number");
+                    long mnum = Convert.ToInt64(Console.ReadLine());
+                    Queue<Customer> temp = new Queue<Customer>(); // temporary queue
+                    _ = cust.RemoveCustomerDetails(mnum);
+                    /*Console.WriteLine(temp.Dequeue().CustomerName, temp.Dequeue().MobileNumber, temp.Dequeue().City, temp.Dequeue().Service);*/
+                    /*foreach( Customer customer in temp )
+                    {
+                        Console.WriteLine(customer.CustomerName, customer.MobileNumber, customer.City, customer.Service);
+                    }*/
+                    if (CustomerQueue.Count == 0)
+                        System.Console.WriteLine("No data found");
+                    else
+                    {
+                        foreach (Customer item in CustomerQueue)
+                        {
+                            System.Console.WriteLine("{0} {1} {2} {3}",item.CustomerName, item.MobileNumber, item.City, item.Service);
+                        }
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("Thank you");
+                    return;
+                default:
+                    Console.WriteLine("Wrong choice");
+                    break;
+
+            }
+        }
+
+    }
+}
