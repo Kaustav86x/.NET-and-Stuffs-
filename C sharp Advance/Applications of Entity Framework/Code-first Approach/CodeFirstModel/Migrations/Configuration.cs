49b07f4defe0fdf8d2c6772e79b322fb68efa149
Kaustav <kaustav86x@gmail.com>
2023-05-08 22:11:56 +0530

    Different applications of Code First Approach

---
+++ C sharp Advance/Applications of Entity Framework/Code-first Approach/CodeFirstModel/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeFirstModel.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstModel.PlutoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirstModel.PlutoContext context) // initial-catalog - CodeFirstDemo
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
