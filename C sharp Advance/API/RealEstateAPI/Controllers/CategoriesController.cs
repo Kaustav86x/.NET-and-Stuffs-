cfe9c989e01c3a593cee96cb7ce26cf511acea3f
Kaustav <kaustav86x@gmail.com>
2023-05-13 14:26:27 +0530

    Using REST API

---
+++ C sharp Advance/API/RealEstateAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateAPI.Models;
+
+namespace RealEstateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private static List<Category> categories = new List<Category>()
+        {
+            new Category { Id = 1, Name = "A1", ImageUrl = "A1.png"},
+            new Category { Id = 2, Name = "A2", ImageUrl = "A2.png"},
+            new Category { Id = 3, Name = "A3", ImageUrl = "A4.pmg"}
+        };
+
+        [HttpGet] //api/categories
+        public IEnumerable<Category> GetCategories() 
+        {
+            return categories; 
+        }
+
+        [HttpPost]
+        public void PostCategories([FromBody] Category cat)
+        {
+            categories.Add(cat);
+        }
+    }
+}
