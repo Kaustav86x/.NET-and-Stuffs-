33f40646ae89cbbd5d49ee5c50e2fb6b760c7b3a
Kaustav <kaustav86x@gmail.com>
2023-05-14 01:57:39 +0530

    Implementing Validation in REST api

--- C sharp Advance/API/RealEstateAPI/Models/Category.cs
+++ C sharp Advance/API/RealEstateAPI/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace RealEstateAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateAPI.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name must not be null")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "ImageUrl must not be null")]
         public string ImageUrl { get; set; }
     }
 }
