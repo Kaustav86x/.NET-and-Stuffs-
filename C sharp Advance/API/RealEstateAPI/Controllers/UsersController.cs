02b8faad8383202e0ab4c9feff32c48bc5a4b3c9
Kaustav <kaustav86x@gmail.com>
2023-05-14 16:38:04 +0530

    Login method implemented

--- C sharp Advance/API/RealEstateAPI/Controllers/UsersController.cs
+++ C sharp Advance/API/RealEstateAPI/Controllers/UsersController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RealEstateAPI.Data;
 using RealEstateAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace RealEstateAPI.Controllers
 {
@@ -10,6 +14,13 @@ namespace RealEstateAPI.Controllers
     public class UsersController : ControllerBase
     {
         APIDbContext _dbcontext = new APIDbContext();
+        // let us use all the fucntionalities of appsettings.json file
+        private IConfiguration _config;
+
+        public UsersController(IConfiguration config)
+        {
+            _config = config;
+        }
 
         [HttpPost("[action]")]
         public IActionResult Register([FromBody] User user)
@@ -35,5 +46,32 @@ namespace RealEstateAPI.Controllers
             return _dbcontext.Users;
         }
 
+        [HttpPost("[action]")]
+        public IActionResult Login([FromBody] User user)
+        {
+            var currUser = _dbcontext.Users.FirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+            if (currUser == null) 
+            {
+                return NotFound();
+            }
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            // hashing our security key
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+            // payloads
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            // generating the JWT token
+            var token = new JwtSecurityToken(
+                issuer: _config["JWT:Issuer"],
+                audience: _config["JWT:Audience"],
+                claims:claims,
+                expires:DateTime.Now.AddMinutes(60),
+                signingCredentials:credentials);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(jwt);
+        }
     }
 }
