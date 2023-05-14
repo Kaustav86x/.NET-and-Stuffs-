65652184cd0a9229fee54cae24452019865040cc
Kaustav <kaustav86x@gmail.com>
2023-05-15 01:10:47 +0530

    PropertiesController.cs is updated !

--- C sharp Advance/API/RealEstateAPI/Controllers/PropertiesController.cs
+++ C sharp Advance/API/RealEstateAPI/Controllers/PropertiesController.cs
@@ -13,6 +13,58 @@ namespace RealEstateAPI.Controllers
     {
         APIDbContext _dbcontext = new APIDbContext();
 
+        [HttpGet("PropertyList")]
+        [Authorize]
+        // finding property of a certain type (ex: catId)
+        public IActionResult GetProperties(int catId) 
+        {
+            var propertiesResult = _dbcontext.Properties.Where(c => c.CategoryId == catId);
+            // if no data being selected 
+            if(propertiesResult == null)
+                return NotFound();
+            else
+                return Ok(propertiesResult);
+        }
+
+        [HttpGet("PropertyDetail")]
+        [Authorize]
+        // finding property of a certain type (ex: catId)
+        public IActionResult GetPropertyDetail(int Id)
+        {
+            var propertiesResult = _dbcontext.Properties.FirstOrDefault(p => p.Id == Id);
+            // if no data being selected 
+            if (propertiesResult == null)
+                return NotFound();
+            else
+                return Ok(propertiesResult);
+        }
+
+
+        [HttpGet("TrendingProperties")]
+        [Authorize]
+        public IActionResult GetTrendingProperties(int Id)
+        {
+            var propertiesResult = _dbcontext.Properties.Where(p => p.IsTrending == true);
+            // if no data being selected 
+            if (propertiesResult == null)
+                return NotFound();
+            else
+                return Ok(propertiesResult);
+        }
+
+        [HttpGet("SearchProperties")]
+        [Authorize]
+        // search by address
+        public IActionResult GetSearchProperties(string address)
+        {
+            var propertiesResult = _dbcontext.Properties.Where(p => p.Address == address);
+            // if no data being selected 
+            if (propertiesResult == null)
+                return NotFound();
+            else
+                return Ok(propertiesResult);
+        }
+
         [HttpPost]
         [Authorize]
         public IActionResult Post([FromBody]Property property) 
