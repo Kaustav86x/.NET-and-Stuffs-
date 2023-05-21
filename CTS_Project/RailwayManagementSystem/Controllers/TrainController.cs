5bf9cab72d690ce7fabbdd2ff01c1f5ae8934530
Kaustav <kaustav86x@gmail.com>
2023-05-22 01:02:13 +0530

    Controllers being added !!!

--- CTS_Project/RailwayManagementSystem/Controllers/TrainController.cs
+++ CTS_Project/RailwayManagementSystem/Controllers/TrainController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RailwayManagementSystem.Data;
 
 namespace RailwayManagementSystem.Controllers
 {
@@ -8,8 +9,30 @@ namespace RailwayManagementSystem.Controllers
     [ApiController]
     public class TrainController : ControllerBase
     {
+        RailwayDbContext _Railwaycontext = new RailwayDbContext();
+
         [HttpGet("TrainList")]
+        // search for a particular train
         // [Authorize] - haven't done yet
+        public IActionResult GetTrainList(int train_id) 
+        {
+            var trainresult = _Railwaycontext.TrainDetails.Where(t => t.Id == train_id);
+            if(trainresult == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(trainresult);
+            }
+        }
+
+        [HttpGet("TrainListByTime")]
+        // fetching train list on the basis of arrival time
+        public IActionResult GetTrainListByTime(DateTime arrival)
+        {
+            
+        }
 
     }
 }
