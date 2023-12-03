0225074390a132c9b85ede7632183c7974ab0160
Kaustav <kaustav86x@gmail.com>
2023-12-04 00:04:45 +0530

    04-12-23
    Domain Models - User and Car are created !!!

---
+++ MVC/RentaCar/Models/Domain/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace RentaCar.Models.Domain
+{
+    public class Car
+    {
+        public Guid CarId { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public double? Kms { get; set; }
+        public double? Rating { get; set; }
+        public string? Description { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ MVC/RentaCar/Models/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿namespace RentaCar.Models.Domain
+{
+    public class User
+    {
+        public Guid USerId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public long? PhoneNo { get; set; }
+
+        public ICollection<Car> Cars { get; set; }
+
+    }
+}
