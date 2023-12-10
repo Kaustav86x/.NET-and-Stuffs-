e12523a81dce77c3ad130cc1460ca3a8682d3ff8
Kaustav <kaustav86x@gmail.com>
2023-12-10 13:45:22 +0530

    10-12-23
    Car controller and the corresponding views are created !!

---
+++ MVC/RentaCar/Controllers/CarController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentaCar.Controllers
+{
+    public class CarController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index() 
+        { 
+            return View();
+        }
+    }
+}
