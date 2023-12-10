2493c9e2315ddaf56aa1a8a45148408d414c5798
Kaustav <kaustav86x@gmail.com>
2023-12-11 00:32:42 +0530

    11-12-23
    Data Binding and Data storing temporarily before sending it to the database !!!

--- MVC/RentaCar/Controllers/CarController.cs
+++ MVC/RentaCar/Controllers/CarController.cs
@@ -1,19 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentaCar.Data;
+using RentaCar.Models.Domain;
+using RentaCar.Models.ViewModels;
 
 namespace RentaCar.Controllers
 {
     public class CarController : Controller
     {
+        private readonly CarDbContext carDbContext;
+        public CarController(CarDbContext carDbContext)
+        {
+            this.carDbContext = carDbContext;
+        }
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Add()
         {
             return View();
         }
 
-        [HttpGet]
-        public IActionResult Index() 
-        { 
-            return View();
+        [HttpPost]
+        [ActionName("Add")]
+        // the Get.cshtml file content would come as an input (acar) to this method
+        public IActionResult AddCar(AddCar acar) 
+        {
+            var car = new Car
+            {
+                Name = acar.Name,
+                Type = acar.Type,
+                Kms = acar.Kms,
+                Rating = acar.Rating,
+                Description = acar.Description,
+            };
+            carDbContext.Cars.Add(car);
+            carDbContext.SaveChanges();
+            /*var name = acar.Name;
+            var type = acar.Type;
+            var kms = acar.Kms;
+            var rating = acar.Rating;
+            var desc = acar.Description;*/
+            return View("Add");
         }
     }
 }
