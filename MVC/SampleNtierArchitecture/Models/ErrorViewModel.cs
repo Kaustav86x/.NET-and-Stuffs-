e38332b277adcf29369796758320e805d8544678
Kaustav <kaustav86x@gmail.com>
2023-12-18 14:15:04 +0530

    18-12-23
    A fresh new .NET MVC project has been created to understand the concepts of N-tier architecture better.

---
+++ MVC/SampleNtierArchitecture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleNtierArchitecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
