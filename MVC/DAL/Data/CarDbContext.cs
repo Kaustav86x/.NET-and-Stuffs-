378a8bfe64d5c0ccef3f7b9a7d0e85c6daa6c526
Kaustav <kaustav86x@gmail.com>
2023-12-24 02:04:09 +0530

    24-12-23
    .

--- MVC/DAL/Data/CarDbContext.cs
+++ MVC/DAL/Data/CarDbContext.cs
@@ -1,14 +1,16 @@
 ﻿using DAL.Model;
 using Microsoft.EntityFrameworkCore;
+/*using Microsoft.EntityFrameworkCore;*/
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Data
 {
-    public class CarDbContext : DbContext
+    public class CarDbContext : System.Data.Entity.DbContext
     {
         public CarDbContext() { }
         public CarDbContext(DbContextOptions options) : base(options) { }
